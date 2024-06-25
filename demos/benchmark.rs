5d60472a503def4eb2553ed62f84e5887bb11145
sokorototo <nyachiengatoto@gmail.com>
2024-06-25 12:06:18 +0300

    added some decent benchmarks

--- demos/benchmark.rs
+++ demos/benchmark.rs
@@ -9,14 +9,13 @@ const HEIGHT: isize = 800;
 const X: isize = -400;
 const Y: isize = -400;
 
-const COUNT: u32 = 50;
+const COUNT: u32 = 30;
 const ITERATIONS: u32 = 10000;
 
 fn main() {
 	// define region
 	let arena = BoundingBox::new(X, Y, WIDTH, HEIGHT);
-	let base = BoundingBox::new(0, 0, WIDTH as _, HEIGHT as _);
-	let mut nodes = (0..COUNT)
+	let nodes = (0..COUNT)
 		.map(|_| {
 			let x = rand_range(X as isize, X as isize + WIDTH as isize);
 			let y = rand_range(Y as isize, Y as isize + HEIGHT as isize);
@@ -27,7 +26,6 @@ fn main() {
 			Node::new(x, y, Some((width, height)))
 		})
 		.collect::<Vec<_>>();
-
 
 	// benchmark
 	let then = Instant::now();
